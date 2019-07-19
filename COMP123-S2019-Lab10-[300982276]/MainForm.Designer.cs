25742c7240ce1107962d75b222423e68bf638dea
simon gonalez <scdg98@outlook.com>
2019-07-19 15:07:20 -0400

    Completed splash screen

---
+++ COMP123-S2019-Lab10-[300982276]/MainForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace COMP123_S2019_Lab10__300982276_
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NextMainButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(233, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to Main Form!";
+            // 
+            // NextMainButton
+            // 
+            this.NextMainButton.Location = new System.Drawing.Point(650, 505);
+            this.NextMainButton.Name = "NextMainButton";
+            this.NextMainButton.Size = new System.Drawing.Size(122, 44);
+            this.NextMainButton.TabIndex = 1;
+            this.NextMainButton.Text = "Next";
+            this.NextMainButton.UseVisualStyleBackColor = true;
+            this.NextMainButton.Click += new System.EventHandler(this.NextMainButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.NextMainButton);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main form";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button NextMainButton;
+    }
+}
+
---
+++ COMP123-S2019-Lab10-[300982276]/MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2019_Lab10__300982276_
+{
+    public partial class MainForm : Form
+    {
+        public Form parent { get; set; }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void NextMainButton_Click(object sender, EventArgs e)
+        {
+            EndForm endForm = new EndForm();
+            endForm.parent = this;
+            endForm.Show();
+            this.Hide();
+            //this.parent.Show();
+            //this.Hide();
+
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //this.parent.Show();
+            //this.Hide();
+
+            Application.Exit();
+        }
+    }
+}
